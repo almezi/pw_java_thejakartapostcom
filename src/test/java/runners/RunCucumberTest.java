96d6fba010e270327a637d436dec616a72c2239e
Aulia Mei Azizi <auliameiazizi@gmail.com>
2026-06-22 14:06:49 +0700

    Add Hooks for teardown and update tests with assertions and glue configuration

--- src/test/java/runners/RunCucumberTest.java
+++ src/test/java/runners/RunCucumberTest.java
@@ -11,7 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps,support")
 @ConfigurationParameter(
         key = PLUGIN_PROPERTY_NAME,
         value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json"
--- src/test/java/steps/HomePageSteps.java
+++ src/test/java/steps/HomePageSteps.java
@@ -7,27 +7,24 @@ import io.cucumber.java.en.Then;
 import pages.HomePageLocators;
 import support.PlaywrightFactory;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 //import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HomePageSteps {
     private final Page page = PlaywrightFactory.getPage();
-    private HomePageLocators homePageLocators = new HomePageLocators();
+    private final HomePageLocators homePageLocators = new HomePageLocators();
     @Given("I open The Jakarta Post website")
     public void iOpenTheJakartaPostWebsite() {
         page.navigate("https://thejakartapost.com/");
-        throw new PendingException();
     }
 
     @Then("the page title should contain {string}")
     public void thePageTitleShouldContain(String expectedText) {
         String actualText = homePageLocators.getJakartaPostLogo();
 
-//        assertEquals(
-//                expectedText,
-//                actualText,
-//                "Jakarta Post logo text is not correct"
-//        );
-        throw new PendingException();
+        assertThat(page.locator(homePageLocators.getJakartaPostLogo()).first())
+                .containsText(expectedText);
     }
 }
